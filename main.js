bd2fc0a997995a083bc01a9f1c808a30b5f391e3
anclah <ansila1230@gmail.com>
2024-01-26 21:42:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+writeExp=(e)=>{
+    display.value+=e.target.id
+    }
+    getResult=()=>{
+        if (display.value==""){
+            alert("Enter an Expression!!")
+        }
+        else{
+            try{
+                display.value=eval(display.value)
+            }
+            catch{
+                alert("Enter valid Expression!!")
+                display.value=""
+            }
+        }
+    }
+    allClear=()=>{
+        display.value=""
+    }
+    deLete=()=>{
+        display.value=display.value.slice(0,-1)
+    }
